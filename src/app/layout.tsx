1f49dd974d8669b4d9bbd6f763b2a665caa8d971
jair1c <gabrieljair.gc@gmail.com>
2026-04-15 17:49:36 -0500

    Ajuste de colores y fecha

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: "20 Rosas para Aracely 🌹",
     description: "Un detalle especial para una persona increíble. Abre el baúl a la medianoche.",
-    url: "https://tu-proyecto.vercel.app", // Podrás actualizar esto luego
+    url: "https://20-rosas.vercel.app/", // Podrás actualizar esto luego
     siteName: "Regalo 20 Rosas",
     images: [
       {
-        url: "/rosas.jpg",
+        url: "/rosas.png",
         width: 1200,
         height: 630,
         alt: "20 Rosas Rojas para Aracely",
